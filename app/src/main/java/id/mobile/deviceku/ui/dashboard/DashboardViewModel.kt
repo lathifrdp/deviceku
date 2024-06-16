808e21c34257f088e3d1c48f8625130ba19f9bc4
lathifrdp <lathifrdp@gmail.com>
2024-06-16 23:37:36 +0700

    add dashboard viewmodel

--- app/src/main/java/id/mobile/deviceku/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/id/mobile/deviceku/ui/dashboard/DashboardViewModel.kt
@@ -1,10 +1,31 @@
 package id.mobile.deviceku.ui.dashboard
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import id.mobile.deviceku.model.DeviceResponseModel
+import id.mobile.deviceku.repository.DeviceRepository
+import kotlinx.coroutines.launch
 
 class DashboardViewModel : ViewModel() {
+
+    private val repository = DeviceRepository()
+    private val _listDevice = MutableLiveData<List<DeviceResponseModel>>()
+    val listDevice: LiveData<List<DeviceResponseModel>> = _listDevice
+
+    fun getListDevice() {
+        viewModelScope.launch {
+            try {
+                val data = repository.getDevice()
+                _listDevice.value = data
+            } catch (e: Exception) {
+                // Handle error
+                Log.e("getListDevice", e.message.toString());
+            }
+        }
+    }
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"

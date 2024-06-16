45ec5664ca17d9699879435766414d04cd756aed
lathifrdp <lathifrdp@gmail.com>
2024-06-16 18:55:08 +0700

    add retrofit

---
+++ app/src/main/java/id/mobile/hapeku/client/Client.kt
@@ -0,0 +1,15 @@
+package id.mobile.hapeku.client
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Client {
+    val baseUrl = "https://api.restful-api.dev/"
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
